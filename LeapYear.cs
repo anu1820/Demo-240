1b3432e776a1331fabea58da47b313e00d6b57ad
anu1820 <anurajpatil35@gmail.com>
2022-12-24 10:31:39 +0530

    Adding LeapYear file

---
+++ LeapYear.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class LeapYear
+    {
+        public static void function() 
+        {
+            Console.WriteLine("Enter the number to find if it is leap year or not.");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if (year % 4== 0) 
+            {
+                Console.WriteLine("This is a leap year");
+            }
+            else
+            {
+                Console.WriteLine("This is not a leap year");
+            }
+
+        }
+    }
+}
